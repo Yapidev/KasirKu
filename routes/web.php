d4124d355ed783a5a18145484234ab81444058a1
Yapidev <88489723+Yapidev@users.noreply.github.com>
2024-11-26 23:35:23 +0700

    add: template files dan login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Public Route
 Route::get('/', function () {
-    return view('welcome');
+    return to_route('login');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('login', App\Livewire\Auth\Login::class)->name('login');
 });
